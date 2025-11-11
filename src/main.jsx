9856e9b0f69381eb03bd778eb7b84a829fb55053
alfred3ks <alfred3ks@gmail.com>
2025-11-11 18:52:09 +0100

    Cambio nombre carpeta de css por styles.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 
-import './css/common.css';
+import './styles/common.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
