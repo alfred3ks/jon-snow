dff72942dfa6874b2c64634d541672900c5a700c
alfred3ks <alfred3ks@gmail.com>
2025-11-17 04:40:48 +0100

    feat: I create the footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Layout from './components/Layout';
 import Bio from './components/Bio';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Layout>
       <Header />
       <Bio />
+      <Footer />
     </Layout>
   );
 };
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import data from '../../data/cv.json';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  const dateNow = new Date();
+  const now = dateNow.getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Copyright ©{now}- {data.name}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
