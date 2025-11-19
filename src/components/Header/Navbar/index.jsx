fcef148a9d431a089ce074d8277cd7599418f671
alfred3ks <alfred3ks@gmail.com>
2025-11-19 10:31:50 +0100

    feat: Add a function to close the menu when clicking on the links.

--- src/components/Header/Navbar/index.jsx
+++ src/components/Header/Navbar/index.jsx
@@ -5,6 +5,7 @@ import Hamburger from './Hamburger';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen((prev) => !prev);
+  const closedMenu = () => setIsOpen((prev) => !prev);
 
   return (
     <nav className={styles.nav}>
@@ -12,16 +13,16 @@ const Navbar = () => {
         <li className={styles.list}>
           <a href="/">Inicio</a>
         </li>
-        <li className={styles.list}>
+        <li className={styles.list} onClick={closedMenu}>
           <a href="#que-hago">Que hago</a>
         </li>
-        <li className={styles.list}>
+        <li className={styles.list} onClick={closedMenu}>
           <a href="#quien-soy">Quien soy</a>
         </li>
-        <li className={styles.list}>
+        <li className={styles.list} onClick={closedMenu}>
           <a href="#batallas">Batallas</a>
         </li>
-        <li className={styles.list}>
+        <li className={styles.list} onClick={closedMenu}>
           <a href="#contacto">Contacto</a>
         </li>
       </ul>
