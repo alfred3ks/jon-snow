de7aa88b5cd8d87d6932a399651f8ae390f9c807
alfred3ks <alfred3ks@gmail.com>
2025-11-19 10:15:55 +0100

    style: added styles to the occupation component.

--- src/components/Occupation/index.jsx
+++ src/components/Occupation/index.jsx
@@ -1,34 +1,38 @@
 import data from '../../data/cv.json';
+import styles from './Occupation.module.css';
 
 const Occupation = () => {
   return (
-    <section>
-      <h3>A que me dedico</h3>
-      {data.occupation.map(({ name, id, description, images }) => {
-        return (
-          <article key={id}>
-            <picture>
-              {images.map((img, id) => {
-                return img.media ? (
-                  <source key={id} srcSet={img.src} media={img.media} />
-                ) : null;
-              })}
-              <img
-                src={
-                  images.find((img) => !img.media)?.src ||
-                  images[images.length - 1].src
-                }
-                alt={
-                  images.find((img) => !img.media)?.alt ||
-                  images[images.length - 1].alt
-                }
-              />
-            </picture>
-            <h3>{name}</h3>
-            <p>{description}</p>
-          </article>
-        );
-      })}
+    <section className={styles.occupation} id="que-hago">
+      <h3 className={styles.title}>A que me dedico</h3>
+      <div className={styles.container}>
+        {data.occupation.map(({ name, id, description, images }) => {
+          return (
+            <article key={id} className={styles.card}>
+              <picture className={styles.picture}>
+                {images.map((img, id) => {
+                  return img.media ? (
+                    <source key={id} srcSet={img.src} media={img.media} />
+                  ) : null;
+                })}
+                <img
+                  className={styles.images}
+                  src={
+                    images.find((img) => !img.media)?.src ||
+                    images[images.length - 1].src
+                  }
+                  alt={
+                    images.find((img) => !img.media)?.alt ||
+                    images[images.length - 1].alt
+                  }
+                />
+              </picture>
+              <h3 className={styles.subTitle}>{name}</h3>
+              <p className={styles.description}>{description}</p>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
