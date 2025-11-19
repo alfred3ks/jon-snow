390a53bad467efb9e782af7dd049c246bc94c7aa
alfred3ks <alfred3ks@gmail.com>
2025-11-19 16:34:06 +0100

    style: He added the styles to history component.

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -1,31 +1,42 @@
 import data from '../../data/cv.json';
+import styles from './History.module.css';
 
 const History = () => {
   const { description, images, skills } = data.history;
   const fallbackImage = images[images.length - 1];
 
   return (
-    <section id="quien-soy">
-      <h3>Mi historia</h3>
-      <article>
-        <div>
+    <section id="quien-soy" className={styles.history}>
+      <h3 className={styles.title}> Mi historia</h3>
+      <article className={styles.container}>
+        <div className={styles.description}>
           {description.map((content, id) => (
-            <p key={id}>{content}</p>
+            <p key={id} className={styles.paragraph}>
+              {content}
+            </p>
           ))}
         </div>
-        <div>
-          <picture>
+        <div className={styles.info}>
+          <picture className={styles.picture}>
             {images.map(({ id, src, media }) =>
               media ? <source key={id} srcSet={src} media={media} /> : null
             )}
-            <img src={fallbackImage.src} alt={fallbackImage.alt} />
+            <img
+              className={styles.image}
+              src={fallbackImage.src}
+              alt={fallbackImage.alt}
+            />
           </picture>
-          {skills.map(({ name, id }) => (
-            <div key={id}>
-              <div>{name}</div>
-              <div>Barra</div>
-            </div>
-          ))}
+          <div className={styles.skillContainer}>
+            {skills.map(({ name, id }) => (
+              <div key={id} className={styles.skillItem}>
+                <div className={styles.skillName}>{name}</div>
+                <div className={`${styles.skillBar}`}>
+                  <div className={`${styles[`skillBar-${id}`]}`}></div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </article>
     </section>
