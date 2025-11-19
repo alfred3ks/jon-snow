72362670903acfc5a64a00f6bebbedb69b7ce01c
alfred3ks <alfred3ks@gmail.com>
2025-11-19 13:35:07 +0100

    refactor: Refactoring of the occupation component.

--- src/components/Occupation/index.jsx
+++ src/components/Occupation/index.jsx
@@ -2,29 +2,25 @@ import data from '../../data/cv.json';
 import styles from './Occupation.module.css';
 
 const Occupation = () => {
+  const { occupation } = data;
   return (
     <section className={styles.occupation} id="que-hago">
       <h3 className={styles.title}>A que me dedico</h3>
       <div className={styles.container}>
-        {data.occupation.map(({ name, id, description, images }) => {
+        {occupation.map(({ name, id, description, images }) => {
+          const fallbackImage = images[images.length - 1];
           return (
             <article key={id} className={styles.card}>
               <picture className={styles.picture}>
-                {images.map((img, id) => {
-                  return img.media ? (
+                {images.map((img, id) =>
+                  img.media ? (
                     <source key={id} srcSet={img.src} media={img.media} />
-                  ) : null;
-                })}
+                  ) : null
+                )}
                 <img
                   className={styles.images}
-                  src={
-                    images.find((img) => !img.media)?.src ||
-                    images[images.length - 1].src
-                  }
-                  alt={
-                    images.find((img) => !img.media)?.alt ||
-                    images[images.length - 1].alt
-                  }
+                  src={fallbackImage.src}
+                  alt={fallbackImage.alt}
                 />
               </picture>
               <h3 className={styles.subTitle}>{name}</h3>
